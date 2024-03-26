4a18aafbdab8c8c9ee6bc5812fa4fc476dc7f131
Vaibhav Arun Khairnar <121480992+King4424@users.noreply.github.com>
2024-03-26 22:31:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,86 +1,39 @@
-import streamlit
-import pandas
-import requests
-import snowflake.connector
-from urllib.error import URLError
-streamlit.title('🥣My Parents New Healthy Diner')
-streamlit.header('🥗Breakfast menu')
-streamlit.text('🐔omega 3 & blueberry Oatmeal')
-streamlit.text('🥑kale, Spinach & rocket smoothie')
-streamlit.text('🍞hard-boiled free-range egg')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
- 
-#import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
-my_fruit_list = my_fruit_list.set_index('Fruit')
- 
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
- 
-# Display the table on the page.
-streamlit.dataframe(fruits_to_show)
- 
+import streamlit as st
+import pandas as pd
+import snowflake.connector as snowflake
 
+# Snowflake connection parameters
+snowflake_user = 'vaibhavk'
+snowflake_password = 'King@4424'
+snowflake_account = 'LFAQUDB-HF93281'
+snowflake_warehouse = 'vaibhav'
+snowflake_database = 'MANGO_LEAF_ML_DB'
+snowflake_schema = 'ML_APP_SCHEMA'
 
+# Snowflake connection
+conn = snowflake.connect(
+    user=snowflake_user,
+    password=snowflake_password,
+    account=snowflake_account,
+    warehouse=snowflake_warehouse,
+    database=snowflake_database,
+    schema=snowflake_schema
+)
 
-#create the repeatable code block (called a funcation)
-def get_fruityvice_data(this_fruit_choice):
-   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-   return fruityvice_normalized   
-#New Section 
-streamlit.header("Fruityvice Fruit Advice!")
-try: 
-   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-   if not fruit_choice:
-     streamlit.error("Please select a fruit to get information.")
-   else:
-     back_from_function = get_fruityvice_data(fruity_choice)
-     streamlit.dataframe(back_from_function)
-#import requests
-     #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-     #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-     #streamlit.dataframe(fruityvice_normalized)
-except URLError as e:
-   streamlit.error()
-streamlit.header("The fruit load list contains:")
-#Snowflake-related functions
-def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
-         my_cur.execute("select * from fruit_load_list")
-         return my_cur.fetchall()
-        
- 
-# write your own comment -what does the next line do? 
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-#streamlit.dataframe(fruityvice_normalized)
- 
-# don't run anything past here while we troubleshoot
-#streamlit.stop()
-#import snowflake.connector
-#Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
-    #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-#my_cur.execute( "select * from pc_rivery_db.public.fruit_load_list" )
-    my_data_rows = get_fruit_load_list()
-#streamlit.header("The fruit load list contains:")
-    streamlit.dataframe( my_data_rows )
-# Allow the end user to add a fruit to the list
-def insert_row_snowflake(new_fruit):
-    with my_cnx.cursor() as my_cur:
-         my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-         return "Thanks for adding " + new_fruit
-add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-if streamlit.button('Add a Fruit to the List'):
-        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-        back_from_function = insert_row_snowflake(add_my_fruit)
-        streamlit.text(back_from_function)
-streamlit.stop() 
-fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', fruit_choice)
- 
-#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+# Function to fetch images data from Snowflake external stage
+def fetch_images_data(folder_name):
+    query = f"SELECT * FROM @MANGO_LEAF_ML_DB.ML_APP_SCHEMA.MANGO WHERE folder = '{folder_name}'"
+    df = pd.read_sql_query(query, conn)
+    return df
+
+# Streamlit app
+st.title('AWS S3 Images Viewer')
+
+# Dropdown to select folder
+selected_folder = st.selectbox('Select a folder:', ['Sooty Mould', 'Powdery Mildew', 'Healthy', 'Gall Midge', 'Die Back', 'Cutting Weevil', 'Bacterial Canker', 'Anthracnose'])
+
+# Fetch images data based on selected folder
+images_df = fetch_images_data(selected_folder)
+
+# Display images data
+st.write(images_df)
