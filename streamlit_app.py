be76f4e8af4e958098f9b822178698c42c9f9cb9
Vaibhav Arun Khairnar <121480992+King4424@users.noreply.github.com>
2024-03-26 23:07:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,11 @@
 import streamlit as st
 import pandas as pd
-import snowflake.connector as snowflake
-
-# Snowflake connection parameters
-snowflake_user = 'vaibhavk'
-snowflake_password = 'King@4424'
-snowflake_account = 'LFAQUDB-HF93281'
-snowflake_warehouse = 'vaibhav'
-snowflake_database = 'MANGO_LEAF_ML_DB'
-snowflake_schema = 'ML_APP_SCHEMA'
-
-# Snowflake connection
-conn = snowflake.connect(
-    user=snowflake_user,
-    password=snowflake_password,
-    account=snowflake_account,
-    warehouse=snowflake_warehouse,
-    database=snowflake_database,
-    schema=snowflake_schema
-)
 
 # Function to fetch images data from Snowflake external stage
-def fetch_images_data(folder_name):
-    query = f"SELECT * FROM @MANGO_LEAF_ML_DB.ML_APP_SCHEMA.MANGO WHERE folder = '{folder_name}'"
-    df = pd.read_sql_query(query, conn)
+def fetch_images_data(folder_name, image_name, snowflake_client):
+    query = f"SELECT * FROM @MANGO_LEAF_ML_DB.ML_APP_SCHEMA.MANGO WHERE folder = '{folder_name}' AND image_name = '{image_name}'"
+    result = snowflake_client.execute(query)
+    df = pd.DataFrame(result.fetchall(), columns=result.column_names)
     return df
 
 # Streamlit app
@@ -33,7 +15,19 @@ st.title('AWS S3 Images Viewer')
 selected_folder = st.selectbox('Select a folder:', ['Sooty Mould', 'Powdery Mildew', 'Healthy', 'Gall Midge', 'Die Back', 'Cutting Weevil', 'Bacterial Canker', 'Anthracnose'])
 
 # Fetch images data based on selected folder
-images_df = fetch_images_data(selected_folder)
+images_df = fetch_images_data(selected_folder, '', snowflake_client)  # Assuming 'snowflake_client' is the Snowflake client object
 
-# Display images data
-st.write(images_df)
+# Get unique image names for the selected folder
+image_names = images_df['image_name'].unique().tolist()
+
+# Dropdown to select image
+selected_image = st.selectbox('Select an image:', [''] + image_names)  # Include an empty option for no image selected
+
+if selected_image:
+    # Fetch images data based on selected folder and image
+    selected_image_df = fetch_images_data(selected_folder, selected_image, snowflake_client)
+    
+    # Display selected image data
+    st.write(selected_image_df)
+else:
+    st.warning('Please select an image to view its data.')
